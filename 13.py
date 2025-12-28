2b3ace5ce7cef4c8a95f933eb70c8fe6e1bcf18c
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2025-12-28 09:45:17 +0530

    Added Python program
    Create a list of food items and print them with indices.

---
+++ 13.py
@@ -0,0 +1,6 @@
+#creating a list with 6 different food names
+list_food_items=["pizza","sushi","burger","noodles","sweeet","chicken"]
+index=1
+for food  in list_food_items:
+    print(f"{index}.{food}")
+    index += 1
