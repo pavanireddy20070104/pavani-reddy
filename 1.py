1af61739159caed3f1e6c0f65213b66542e8bd4c
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2025-12-28 08:59:40 +0530

    Added Python program
    Added variables for name, age, and height, and included user input for student status.

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
-
+#creating 4 variables of different data types
+name="Pavani Reddy"
+age=18
+height=5.3
+print(name)
+print(age)
+print(height)
+student=input("enter whether you are student or not?(true/false):")
+if student=="true":
+    print("you are  a student")
+else:
+    print("you are not a student")
