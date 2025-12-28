1b19078b5c18f1ed5f49b4c8a4921978c6910de0
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2025-12-28 21:27:01 +0530

    Added Python program

--- 16.py
+++ 16.py
@@ -1,5 +1,5 @@
 #creating a list of numbers from 1 to 20 numbers
-numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+numbers=list(range(1,21))
 #printing the numbers in the list that are divisible by 3
 for num in numbers:
     if num%3==0:
