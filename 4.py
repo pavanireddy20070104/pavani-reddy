6a5d2879fe13c0ea8fc9de9cc251428985462cdb
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2025-12-28 09:13:38 +0530

    Added Python program

---
+++ 4.py
@@ -0,0 +1,7 @@
+#creating variable with my favourite number
+my_favourite_number=int(input("give  favourite number:"))
+#checking whether my_favorite_number is even or odd by using "%" operator
+if my_favourite_number%2==0:
+    print("my favourite number is even" )
+else:
+    print("my favourite number is odd")
