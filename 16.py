9f81b02acf640914af1984c725bf284bb374f9fe
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2025-12-28 10:02:42 +0530

    Added Python program

---
+++ 16.py
@@ -0,0 +1,8 @@
+#creating a list of numbers from 1 to 20 numbers
+numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#printing the numbers in the list that are divisible by 3
+for num in numbers:
+    if num%3==0:
+        print("The numbers that are divisible by 3 are",num)
+
+ 
