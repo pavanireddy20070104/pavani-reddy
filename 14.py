93c20a0462300a7119fe7b89fe2cd5961146f018
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2025-12-28 09:47:39 +0530

    Added Python program

---
+++ 14.py
@@ -0,0 +1,6 @@
+#creating an empty list
+favourite_movies=[]
+for i in range(5):
+    movies=input("enter your favourite movies:")
+    favourite_movies.append(movies)
+print("your favourite movies are:",favourite_movies)
