263796e7534f87e3fbe681c1a71144711957ab48
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2025-12-28 09:07:52 +0530

    Added Python program

---
+++ 3.py
@@ -0,0 +1,12 @@
+#creating variables
+price=599.99
+discount=0.15
+#calculating dicount amount
+discount_amount=price*discount
+#final price after getting discount
+final_price=price-discount_amount
+#round of the final price into two decimals
+final_price=round(final_price,2)
+#output
+print("final price after getting discount is:",final_price)
+
