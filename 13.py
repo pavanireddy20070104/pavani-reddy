824757cdc63f714f6458f9a1135fcf94354fdbdd
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2025-12-28 21:39:49 +0530

    Added Python program
    Corrected the spelling of 'sweeet' to 'sweet' in the food list.

--- 13.py
+++ 13.py
@@ -1,5 +1,5 @@
 #creating a list with 6 different food names
-list_food_items=["pizza","sushi","burger","noodles","sweeet","chicken"]
+list_food_items=["pizza","sushi","burger","noodles","sweet","chicken"]
 index=1
 for food  in list_food_items:
     print(f"{index}.{food}")
