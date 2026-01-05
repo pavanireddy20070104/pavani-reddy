0dbb0e6759054de46e5b0bf2ae84df96230f7275
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-05 20:41:40 +0530

    Added Python program
    Generate a list of random integers and find the largest and smallest values.

--- 17.py
+++ 17.py
@@ -1,1 +1,16 @@
-
+import random
+# Create a list of 8 random integers between 1 and 100
+numbers = []
+for i in range(8):
+    numbers.append(random.randint(1, 100))
+print("List:", numbers)
+largest = numbers[0]
+smallest = numbers[0]
+# Find largest and smallest using loop
+for num in numbers:
+    if num > largest:
+        largest = num
+    if num < smallest:
+        smallest = num
+print("Biggest number:", largest)
+print("Smallest number:", smallest)
