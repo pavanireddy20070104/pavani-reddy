769642acc65b410cb24b8d4937f68548fb237202
pavanireddy20070104 <pavanireddypavani019@gmail.com>
2026-01-05 20:43:19 +0530

    Added Python program

--- 20.py
+++ 20.py
@@ -1,1 +1,20 @@
+shopping_list = []
+
+while True:
+    choice = input("add / remove / show / quit: ")
+
+    if choice == "add":
+        item = input("Item: ")
+        shopping_list.append(item)
+
+    elif choice == "remove":
+        item = input("Item: ")
+        if item in shopping_list:
+            shopping_list.remove(item)
+
+    elif choice == "show":
+        print(shopping_list)
+
+    elif choice == "quit":
+        break
 
